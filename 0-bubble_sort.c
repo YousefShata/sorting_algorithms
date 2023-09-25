363d016e1725eecc64d0c9e0d5c83ea34282e581
YousefShata <yossef_shata_a@yahoo.com>
2023-09-25 13:24:19 -0700

    Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array using Bubble Sort
+ * @array: an array of integers
+ * @size: size of the array
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int swap, tmp;
+	unsigned int i;
+
+	while (1)
+	{
+		swap = 0;
+		for (i = 0; i < size; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+				swap = 1;
+			}
+		}
+		if (swap == 0)
+			break;
+	}
+}
