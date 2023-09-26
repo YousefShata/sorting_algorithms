43961a8c26e0965bfca2bf960f2bbd6330e111a1
YousefShata <yossef_shata_a@yahoo.com>
2023-09-25 17:12:28 -0700

    fixing error in selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,8 +8,8 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int tmp, courier;
+	size_t i, j, courier;
+	int tmp;
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -17,10 +17,13 @@ void selection_sort(int *array, size_t size)
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[courier])
+				courier = j;
+
+			if (courier != i)
 			{
 				tmp = array[courier];
-				array[courier] = array[j];
-				array[j] = tmp;
+				array[courier] = array[i];
+				array[i] = tmp;
 				print_array(array, size);
 			}
 		}
---
+++ 3-quick_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * quick_sort - sorting using Quick Sort
+ * @array: array of int
+ * @size: size of the array
+ * Return: return nothing
+ */
+
+void quick_sort(int *array, size_t size)
+{
+
+}
